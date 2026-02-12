86f3dc33c3d61fafa2a2202081e18ce8e58b803c
carlosuni7 <carlos.universidade7@email.com>
2026-02-12 16:06:05 -0300

    Add Exercicio de Objetos

---
+++ Atividades/intermediario/objetos-js/personagem.js
@@ -0,0 +1,16 @@
+
+class Personagem {
+
+    constructor(nome, raca, poder){
+        this.nome,
+        this.raca,
+        this.poder
+    }
+
+    imprimirDescricao() {
+        return `
+        Descrição: ${this.nome}, poder: ${poder}, raça: 
+        `
+    }
+
+}
