c2e445467b74822c29a91c48b744b4f682e246ae
unknown <you@example.com>
2026-02-25 15:58:38 -0300

    add dicionario

--- Python/desafios/verifica-idade.py
+++ Python/desafios/verifica-idade.py
@@ -1,27 +1,53 @@
 listaDePessoas = []
-pessoa = {}
 
 nome = input("Insira um nome: ")
 idade = int(input("Insira sua idade: "))
 
 #Funções devem ser independentes e reutilizáveis.
 def statusfun(idade):
-            if idade >= 18:
-                return "Maior de idade"
-            else:
-                return "Menor de idade"
+    if idade >= 18:
+        return "Maior de idade"
+    else:
+        return "Menor de idade"
                 
-status = statusfun(idade)
-    
-def criar_pessoa(nome, idade, status):
-    pessoa['nome'] = nome
-    pessoa['idade'] = idade
-    pessoa['status'] = status
-    pessoa['universo'] = "Dragon Ball Z"
+
+def criar_pessoa(nomeinput, idadeinput):
+    pessoa = {
+        "nome": nomeinput,
+        "idade": idadeinput,
+        "status": statusfun(idade),
+        "universo": "Dragon Ball Z"
+    }
     return pessoa
-    
-addPessoa = criar_pessoa(nome, idade, status)
 
+addPessoa = criar_pessoa(nome, idade)
 listaDePessoas.append(addPessoa)
-
 print(listaDePessoas)
+
+i = 1
+while i < 2:
+    
+    nome = input("Insira um nome: ")
+    idade = int(input("Insira sua idade: "))
+
+    #Funções devem ser independentes e reutilizáveis.
+    def statusfun(idade):
+        if idade >= 18:
+            return "Maior de idade"
+        else:
+            return "Menor de idade"
+                
+
+    def criar_pessoa(nomeinput, idadeinput):
+        pessoa = {
+        "nome": nomeinput,
+        "idade": idadeinput,
+        "status": statusfun(idade),
+        "universo": "Dragon Ball Z"
+        }
+        return pessoa
+
+    addPessoa = criar_pessoa(nome, idade)
+    listaDePessoas.append(addPessoa)
+    print(listaDePessoas)
+
