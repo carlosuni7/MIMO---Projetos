877910d918391f755faa48cb3d68840223e1fd91
Carlos Gonçalves <carlos.unig7@gmail.com>
2026-03-02 12:42:51 -0300

    Novos exercicios em Py

---
+++ Python/01-Lista-Estrutura-Sequencial/ex-005.py
@@ -0,0 +1,13 @@
+inMetro = 1.80
+inCentimetro = 180
+
+toCentimetro = inMetro * 100
+toMetro = inCentimetro / 100
+
+print(toCentimetro)
+print(toMetro)
+
+print(f"Eu Carlos tenho {toMetro}m de altura em metro")
+print(f"Eu carlos tenho {toCentimetro:.0f}cm de altura em centimetro")
+
+git
---
+++ Python/01-Lista-Estrutura-Sequencial/ex-013.py
@@ -0,0 +1,8 @@
+inNotaUm = float(input("Insira primeira nota: "))
+inNotaDois = float(input("Insira segunda nota: "))
+inNotaTres = float(input("Insira terceira nota: "))
+inNotaQuatro = float(input("Insira quarta nota: "))
+
+resultadoMedia = (inNotaUm + inNotaDois + inNotaTres + inNotaQuatro) / 4
+
+print(f"Resultado da Média: {resultadoMedia}")
