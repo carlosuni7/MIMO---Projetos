215035dcfdb45974c259c65aa44fe53c71aa4e9f
Carlos Gonçalves <carlos.unig7@gmail.com>
2026-03-10 16:16:33 -0300

    Novos exercicios em python feito

--- Python/01-Lista-Estrutura-Sequencial/ex-002.py
+++
@@ -1,3 +0,0 @@
-numero = int(input("Insira um numero: "))
-
-print(f"O número informado foi: {numero}")
---
+++ Python/02-Lista-Estrutura-Decisao/ex-002.py
@@ -0,0 +1,9 @@
+import os
+os.system('cls')
+
+number = int(input('Insira um número: '))
+
+if number > 0:
+    print('Seu número é positivo!-')
+elif number < 0:
+    print('Seu número é negativo!')
