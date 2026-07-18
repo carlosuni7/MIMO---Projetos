03eef256443c5624bf81a38ff36716a94a9f4ef4
Carlos Gonçalves <carlos.unig7@gmail.com>
2026-07-18 14:26:36 -0300

    feat: remover arquivos Python de exemplos de estrutura sequencial

--- Python/01-Lista-Estrutura-Sequencial/ex010.py
+++
@@ -1,8 +0,0 @@
-import os
-os.system('cls')
-
-celsius = float(input("Insira uma temperatura em celsius: "))
-
-farenheit = float( (celsius * (9/5)) + 32 )
-
-resposta = input(f"A temperatura celsius em Farenheit: {farenheit:.1f}F°")
